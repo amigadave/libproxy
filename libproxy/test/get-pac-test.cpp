e5d4b2daaee56e0a9fe7dd99d0f9666ecf5f8b73
David King <amigadave@amigadave.com>
2016-12-31 21:29:38 +0000

    get-pac-test: Fix overflow test
    Rather than checking for EBADF when the socket is closed due to an
    overly-large request, check that the sent size differs from the
    received. Use a large buffer to ensure that the number of bytes sent are
    greater than the kernel TCP receive buffer size, as otherwise the send()
    call succeeds even though the bytes were never retrieved from the
    socket. Pass the MSG_NOSIGNAL flag so that SIGPIPE is not generated when
    the socket is closed once the content length has been read.

--- libproxy/test/get-pac-test.cpp
+++ libproxy/test/get-pac-test.cpp
@@ -188,20 +188,20 @@ done:
 		void sendOverflow(int csock)
 		{
 			int ret;
-			int size = 500000;
+			int size = 50000000; // Must be larger than the kernel TCP receive buffer
 			char *buf = new char[size];
 			memset(buf, 1, size);
 
 			const char *basic =
 				"HTTP/1.1 200 OK\n" \
 				"Content-Type: text/plain\n" \
-				"Content-Length: 500000\n" \
+				"Content-Length: 50000000\n" \
 				"\n";
-			ret = send(csock, (void*)basic, strlen(basic), 0);
+			ret = send(csock, (void*)basic, strlen(basic), MSG_NOSIGNAL);
 			assert(ret == strlen(basic));
-			ret = send(csock, (void*)buf, size, 0);
-			if (!(errno == EBADF))
-				abort(); // Test failed... the socket did not close on us
+			ret = send(csock, (void*)buf, size, MSG_NOSIGNAL);
+			if (ret == size)
+				abort(); // Test failed... the socket should not accept the whole buffer
 			delete[] buf;
 			shutdown(csock, SHUT_RDWR);
 		}
